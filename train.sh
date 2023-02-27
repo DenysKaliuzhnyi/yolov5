53bf22229c0bc8354566031f90800a521c41735c
DenysKaliuzhnyi <denyskaliuzhnyi@gmail.com>
2023-02-27 18:26:11 +0200

    add exist-ok to monuseg train and val scripts

--- train.sh
+++ train.sh
@@ -44,6 +44,7 @@ source env/bin/activate
 
 python train.py --img 512 \
                 --batch 32 \
+                --exist-ok \
                 --workers 4 \
                 --epochs 300 \
                 --patience 0 \
