0c9eea1f4fb45aec18d1a94f9015e94366bb1a35
DenysKaliuzhnyi <denyskaliuzhnyi@gmail.com>
2023-03-31 20:34:14 +0300

    update testis files

--- train_testis.sh
+++ train_testis.sh
@@ -44,6 +44,7 @@ source env/bin/activate
 #yolov5s.pt
 python train.py --img 1024 \
                 --batch 32 \
+                --exist-ok \
                 --workers 4 \
                 --epochs 300 \
                 --patience 0 \
@@ -52,7 +53,7 @@ python train.py --img 1024 \
                 --optimizer Adam \
                 --bbox_interval 10 \
                 --weights yolov5s.pt \
-                --hyp data/hyps/hyp.scratch-med.yaml \
+                --hyp data/hyps/hyp.scratch-testis.yaml \
                 --data "$DATA_FILE_PATH" --name "$EXP_PATH"
 
 echo "DONE"
